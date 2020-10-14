126292cf1e20f11bba84f04979ddc02a98a17c02
renliang <renliang@polarico.com>
2020-10-14 17:44:46 +0800

    Update iris.py

--- 产品大数据分析与建模/bigdata_model/iris.py
+++ 产品大数据分析与建模/bigdata_model/iris.py
@@ -21,8 +21,8 @@ X = iris[["SepalLength","SepalWidth","PetalLength","PetalWidth"]]
 y = iris["Class"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42,stratify=y)
 
-print y_train.value_counts()
-print y_test.value_counts()
+print(y_train.value_counts())
+print(y_test.value_counts())
 
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(C=1e3,solver='lbfgs')
@@ -30,9 +30,9 @@ classifier.fit(X_train, y_train)
 
 from sklearn import metrics
 predict_y = classifier.predict(X_test)
-print metrics.classification_report(y_test,predict_y)
+print(metrics.classification_report(y_test,predict_y))
 
-print "分类正确率: ", metrics.accuracy_score(y_test,predict_y)
+print("分类正确率: ", metrics.accuracy_score(y_test,predict_y))
 
 # import seaborn as sns
 # colorMetrics = metrics.confusion_matrix(y_test,predict_y)
@@ -41,7 +41,7 @@ print "分类正确率: ", metrics.accuracy_score(y_test,predict_y)
 # plt.show()
 
 coef_df = pd.DataFrame(classifier.coef_, columns=iris.columns[0:4])
-print coef_df.round(2)
+print(coef_df.round(2))
 
 coef_df["intercept"] = classifier.intercept_
-print coef_df.round(2)
+print(coef_df.round(2))
